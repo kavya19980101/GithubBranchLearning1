dd8f79816f2801aff391d4725fcea1edadc20a1d
kavya <kavyastestings@gmail.com>
2025-10-25 15:29:26 +0530

    fisrt commit

---
+++ src/test/java/gridpro/Sample1.java
@@ -0,0 +1,27 @@
+package gridpro;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.testng.annotations.Test;
+
+public class Sample1 {
+
+	@Test
+	public void TC01() throws MalformedURLException
+	{
+		URL ipaddress=new URL("");
+		
+		ChromeOptions option=new ChromeOptions();
+		
+//		DesiredCapabilities option=new DesiredCapabilities();
+//		option.setBrowserName("chrome"); 
+		RemoteWebDriver driver=new RemoteWebDriver(ipaddress, option);
+		driver.get("https://www.google.com");
+	 System.out.println(driver.getTitle());	
+	 driver.close();
+	}
+}
